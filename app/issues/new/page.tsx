5432a304e9114bf38437ef5d8e0d515ecc1d7d0e
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-25 10:08:22 +0500

    document reference issue solved by using dynamic rendering

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,19 @@
 
 import 'easymde/dist/easymde.min.css';
 
-import SimpleMDE from 'react-simplemde-editor';
+import dynamic from 'next/dynamic';
 
 import { Button, TextField } from '@radix-ui/themes';
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl px-4 space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE />
+      {<SimpleMDE />}
       <Button>Submit New Issue</Button>
     </div>
   );
