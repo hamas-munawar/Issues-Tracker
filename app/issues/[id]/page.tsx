b561845e4cb863515eacbfc093b2d8ca5ffe7a2e
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-26 13:03:56 +0500

    Added the Edit Button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
-import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
-import { IssueStatusBadge } from '@/app/components';
-import prisma from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { IssueStatusBadge } from "@/app/components";
+import prisma from "@/prisma/client";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
@@ -13,16 +15,24 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   if (!issue) notFound();
 
   return (
-    <Flex direction="column" gapY="2" className="max-w-xl">
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
+    <Grid columns={{ initial: "1", md: "2" }} gap="4">
+      <Flex direction="column" gapY="2" className="max-w-xl">
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
       </Flex>
-      <Card className="prose">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </Flex>
+      <Box className="max-w-xl">
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
