0098d4f2e16493f6a33631a96cfd731fdb689286
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-01 11:45:10 +0500

    feat(AssigneeSelect): add issue assignment functionality
    - Pass issue prop to AssigneeSelect component
    - Implement loading and error states
    - Add default value and onValueChange handler for assignment
    - Include "Unassigned" option in dropdown

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,25 +2,42 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import { User } from "@/app/generated/prisma";
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@/app/generated/prisma";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 
-const AssigneeSelect = () => {
-  const { data: users } = useQuery<User[]>({
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000, // 60s
     retry: 3,
   });
 
+  if (isLoading) return <Skeleton height="2rem" />;
+
+  if (error) return null;
+
   return (
-    <Select.Root defaultValue="hamas">
+    <Select.Root
+      defaultValue={issue.assignedToUserId || "unassigned"}
+      onValueChange={(userId) => {
+        axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: userId === "unassigned" ? null : userId,
+        });
+      }}
+    >
       <Select.Trigger />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
           <Select.Separator />
+          <Select.Item value="unassigned">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
