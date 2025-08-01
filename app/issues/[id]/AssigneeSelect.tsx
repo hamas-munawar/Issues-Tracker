94e358ce0f7e0679d2173744b10f1e4beb7d12f5
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-01 08:08:20 +0500

    Populate Assignee Select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+import { User } from "@/app/generated/prisma";
 import { Select } from "@radix-ui/themes";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root defaultValue="hamas">
       <Select.Trigger />
@@ -10,8 +24,11 @@ const AssigneeSelect = () => {
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
           <Select.Separator />
-          <Select.Item value="hamas">Hamas</Select.Item>
-          <Select.Item value="ali">Ali</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
